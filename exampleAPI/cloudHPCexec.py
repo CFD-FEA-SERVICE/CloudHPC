85bb2d850b7b866cf181a15a144f7a0b5c463387
Ruggero Poletto <ruggero.poletto@cfdfeaservice.it>
2022-02-10 17:40:16 +0100

    Updating to API v2 and beginning of Python interface

---
+++ exampleAPI/cloudHPCexec.py
@@ -0,0 +1,124 @@
+#!/bin/python3
+
+#import PySimpleGUI as sg
+
+import tkinter as tk
+from tkinter import ttk, filedialog
+from pathlib import Path
+import os, json
+
+# dependencies installation
+os.system("pip install touch requests")
+import touch, requests
+
+try:
+    os.makedirs( os.path.join( str( Path.home() ) , '.cfscloudhpc') )
+except FileExistsError:
+    pass
+
+DOTENV_FILE = os.path.join( str( Path.home() ) , '.cfscloudhpc', 'apikey' )
+touch.touch( DOTENV_FILE )
+
+# window definition
+root = tk.Tk()
+
+root.title( "Cloud HPC - Run" )
+root.grid_columnconfigure(0, weight=5)
+root.grid_columnconfigure(1, weight=5)
+root.grid_columnconfigure(2, weight=1)
+
+window_width = 300
+window_height = 200
+
+# get the screen dimension
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+# find the center point
+center_x = int(screen_width/2 - window_width / 2)
+center_y = int(screen_height/2 - window_height / 2)
+
+root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+root.resizable(False,False)
+
+#SEMPRE IN PRIMO PIANO
+root.attributes('-topmost', 1)
+
+#ICONA DELLA FINESTRA
+#root.iconbitmap('./assets/pythontutorial.ico')
+
+#APIKEY
+apikey = tk.StringVar()
+
+env_file = open( DOTENV_FILE, 'r' )
+apikey_file = env_file.readline()
+env_file.close()
+
+apikey.set( apikey_file )
+
+ttk.Label(root, text="APIKEY:").grid( row=3, column=0 )
+ttk.Entry(root, textvariable=apikey, width=30).grid( row=3, column=1, columnspan=2 )
+#apikey_entry.pack(fill='x', expand=True)
+
+#CPU DROP DOWN
+headers = { "X-API-key" : apikey.get().rstrip("\n"), "accept" : "application/json", }
+cpu_response = requests.get( 'https://cloud.cfdfeaservice.it/api/v2/simulation/view-cpu', headers=headers)
+
+cpu_dropdown = tk.StringVar()
+cpu_dropdown.set( cpu_response.json()['response'][0] )
+
+ttk.Label(root, text="vCPU:").grid( row=4, column=0 )
+cpumenu = tk.OptionMenu( root, cpu_dropdown, *cpu_response.json()['response'] )
+cpumenu.grid( row=4, column=1, columnspan=2  )
+cpumenu.config(width=25)
+
+#RAM DROP DOWN
+headers = { 'X-API-key' : apikey.get().rstrip("\n"), 'accept' : 'application/json', }
+ram_response = requests.get( 'https://cloud.cfdfeaservice.it/api/v2/simulation/view-ram', headers=headers)
+
+ram_dropdown = tk.StringVar()
+ram_dropdown.set( ram_response.json()['response'][0] )
+
+ttk.Label(root, text="RAM:").grid( row=5, column=0 )
+rammenu = tk.OptionMenu( root, ram_dropdown, *ram_response.json()['response'] )
+rammenu.grid( row=5, column=1, columnspan=2 )
+rammenu.config(width=25)
+
+#SCRIPT DROP DOWN
+headers = { 'X-API-key' : apikey.get().rstrip("\n"), 'accept' : 'application/json', }
+scripts_response = requests.get( 'https://cloud.cfdfeaservice.it/api/v2/simulation/view-scripts', headers=headers)
+
+scripts_dropdown = tk.StringVar()
+scripts_dropdown.set( scripts_response.json()['response'][0] )
+
+ttk.Label(root, text="SCRIPT:").grid( row=6, column=0 )
+scriptmenu = tk.OptionMenu( root, scripts_dropdown, *scripts_response.json()['response'] )
+scriptmenu.grid( row=6, column=1, columnspan=2 )
+scriptmenu.config(width=25)
+
+#FOLDER
+def getFolderPath():
+    folder_selected = filedialog.askdirectory()
+    folderPath.set(folder_selected)
+
+folderPath = tk.StringVar()
+ttk.Label(root, text="FOLDER:").grid( row=7, column=0 )
+ttk.Entry(root, textvariable=folderPath).grid( row=7, column=1 )
+ttk.Button(root, text="Browse Folder",command=getFolderPath).grid( row=7, column=2)
+
+#BOTTONE
+def select(APIKEY, DOTENV_FILE, cpu, ram, script, path):
+    print(APIKEY)
+    print(cpu)
+    print(ram)
+    print(script)
+    print(path)
+    env_file = open( DOTENV_FILE , 'w')
+    env_file.write( APIKEY )
+    env_file.close()
+
+ButtonOK = ttk.Button(root, text='Launch', command=lambda: select( apikey.get().rstrip("\n"), DOTENV_FILE , cpu_dropdown.get(), ram_dropdown.get(), scripts_dropdown.get(), folderPath.get() ) ).place( x=window_width-160, y=window_height-30 )
+ButtonCancel = ttk.Button(root, text='Cancel', command=root.destroy).place( x=window_width-80, y=window_height-30 )
+
+# keep the window displaying
+root.mainloop()
