7ab04e19ebe74cafa647637b71456fd289d1b916
Ruggero Poletto <ruggero.poletto@cfdfeaservice.it>
2022-03-04 11:08:16 +0100

    v0.1-alpha release

--- exampleAPI/cloudHPCexec.py
+++ exampleAPI/cloudHPCexec.py
@@ -8,7 +8,7 @@ from pathlib import Path
 import os, json
 
 # dependencies installation
-os.system("pip install touch requests")
+#os.system("pip install touch requests")
 import touch, requests, shutil
 
 try:
