85defde9308c0f7e51fd743898c6ca2b5679fcc8
Ruggero Poletto <ruggero.poletto@cfdfeaservice.it>
2025-06-13 17:22:05 +0200

    Updated blender and FDS/SMV versions

--- installFDS.ps1
+++ installFDS.ps1
@@ -7,13 +7,17 @@ $wsh.Popup("Complete anaconda installation process ...")
 Start-Process .\Anaconda3-2024.10-1-Windows-x86_64.exe -NoNewWindow -Wait
 #pip install numpy scipy matplotlib fdsreader argparse cantera
 
-wget "https://github.com/firemodels/fds/releases/download/FDS-6.10.1/FDS-6.10.1_SMV-6.10.1_win.exe" -OutFile "FDS-6.10.1_SMV-6.10.1_win.exe"
+#wget "https://github.com/firemodels/fds/releases/download/FDS-6.10.1/FDS-6.10.1_SMV-6.10.1_win.exe" -OutFile "FDS-6.10.1_SMV-6.10.1_win.exe"
+wget "https://github.com/firemodels/test_bundles/releases/download/FDS_TEST/FDS-6.10.1-334_SMV-6.10.1-199_test_win.exe" -OutFile "FDS-6.10.1-334_SMV-6.10.1-199_test_win.exe"
 $wsh.Popup("Complete FDS installation process ...")
-Start-Process .\FDS-6.10.1_SMV-6.10.1_win.exe -NoNewWindow -Wait
+#Start-Process .\FDS-6.10.1_SMV-6.10.1_win.exe -NoNewWindow -Wait
+Start-Process .\FDS-6.10.1-334_SMV-6.10.1-199_test_win.exe -NoNewWindow -Wait
 
-wget "https://ftp.halifax.rwth-aachen.de/blender/release/Blender4.4/blender-4.4.3-windows-x64.msi" -OutFile "blender-4.4.3-windows-x64.msi"
+#wget "https://ftp.halifax.rwth-aachen.de/blender/release/Blender4.4/blender-4.4.3-windows-x64.msi" -OutFile "blender-4.4.3-windows-x64.msi"
+wget "https://download.blender.org/release/Blender4.1/blender-4.1.1-windows-x64.msi" -OutFile "blender-4.1.1-windows-x64.msi"
 $wsh.Popup("Complete blender installation process ...")
-Start-Process .\blender-4.4.3-windows-x64.msi -NoNewWindow -Wait
+#Start-Process .\blender-4.4.3-windows-x64.msi -NoNewWindow -Wait
+Start-Process .\blender-4.1.1-windows-x64.msi -NoNewWindow -Wait
 
 wget "https://github.com/firetools/blenderfds/releases/download/v6.0.0/blenderfds.zip" -OutFile "blenderfds.zip"
 $wsh.Popup("Follow instructions in the browser to complete blenderFDS installation on blender")
